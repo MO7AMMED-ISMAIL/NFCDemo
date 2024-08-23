be5d784c8f9d8e06e928819afaa3fd5be774ebd1
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2024-08-23 20:38:39 +0300

    vercel

--- src/NFC.jsx
+++ src/NFC.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NFCComponent = () => {
     const [message, setMessage] = useState('');
-    const [nfcUrl, setNfcUrl] = useState('https://www.linkedin.com/in/mohammed-ismail-6b8931275/');
+    const [nfcUrl, setNfcUrl] = useState('https://tapnet.pages.dev/ali');
 
     const writeToNFC = async () => {
         try {
