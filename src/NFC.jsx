f9079cb22dfc4a6bae6236fddabb27a6f0d63837
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2024-08-23 23:21:43 +0300

    vercel

--- src/NFC.jsx
+++ src/NFC.jsx
@@ -4,30 +4,45 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const NFCComponent = () => {
     const [message, setMessage] = useState('');
     const [nfcUrl, setNfcUrl] = useState('https://tapnet.pages.dev/ali');
+    const [scanning , setScanning] = useState(false);
 
     const writeToNFC = async () => {
+        setScanning(!scanning);
         try {
             const ndef = new window.NDEFReader();
             await ndef.write({
                 records: [{ recordType: 'url', data: nfcUrl }],
             });
-            alert('URL written to NFC tag! sucessfully');
+            setMessage("NFC Card Open the url successfully... ");
         } catch (error) {
             console.error('Error writing to NFC tag:', error);
             alert('Cannot write to NFC tag: ' + error.message);
         }
+        setScanning(!scanning);
     };
 
     return (
         <div className="container text-center mt-5">
             <h1>NFC Demo</h1>
-            <button 
-                className="btn btn-primary mt-3" 
-                onClick={writeToNFC}
-            >
-                Write to NFC
-            </button>
-            {message && <p className="mt-3">{message}</p>}
+            {
+                scanning ? (
+                    <button 
+                        className="btn btn-primary mt-3" 
+                        onClick={writeToNFC}
+                    >
+                        Scan NFC Card
+                    </button>
+                ):(
+                    <>
+                        <h1>PLZ Near NFC Card From mobile to scan</h1>
+                        <div class="spinner-border" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </>
+                )
+            }
+            
+            {message && <p className="mt-3 text-success">{message}</p>}
         </div>
     );
 };
